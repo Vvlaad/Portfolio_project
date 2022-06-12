12aca9202758fdef3fec1dadf58c8d325ebcec92
Vladislav <AidenPride@protonmail.com>
2022-06-12 17:38:08 +0300

    five sections done

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,10 @@ closeElem .addEventListener('click', () => {
     menu.classList.remove('active')
 });
 
+const percentages = document.querySelectorAll('.skill__percentage'),
+      scales = document.querySelectorAll('.skill__scale .skill__scale-fullness');
+      
+percentages.forEach( (item, i) => {
+    scales[i].style.width = item.innerHTML;
+});
+
